6752dc9f9a95de8247b2ba713861a74e43e55d39
jialipeng <jialipeng@baidu.com>
2018-08-22 00:11:27 +0800

    menu

--- src/pages/doc/components/docs/viewpoint.vue
+++ src/pages/doc/components/docs/viewpoint.vue
@@ -11,7 +11,6 @@
     </div>
 </template>
 <style lang="less">
-
 .doc-page {
     .component-list {
         width: 200px;
@@ -27,37 +26,49 @@
 
 <script>
 import list from '@/pages/doc/common/config/menu.json';
-console.log('list', list);
+const convertDts = o => Object.entries(o).map(([k, v]) => {
+    const d = {
+        title: v.name || k,
+        key: k
+    };
+    let child = null;
+    v.subList && (child = convertDts(v.subList));
+    child && (d.children = child);
+    return d;
+});
+const menuList = convertDts(list);
+
 export default {
     data() {
         return {
-            menuList: [{
-                title: '收藏',
-                key: 'favor',
-                icon: 'ui-icon-star',
-                count: 20,
-                children: [
-                    {
-                        title: '类型-1',
-                        count: 100,
-                        key: '2-1'
-                    },
-                    {
-                        title: '类型-2',
-                        key: '3-2',
-                        children: [
-                            {
-                                title: '类型-2-3',
-                                key: '2-3'
-                            },
-                            {
-                                title: '类型-2-4',
-                                key: '3-4'
-                            }
-                        ]
-                    }
-                ]
-            }]
+            menuList
+            // menuList: [{
+            //     title: '收藏',
+            //     key: 'favor',
+            //     icon: 'ui-icon-star',
+            //     count: 20,
+            //     children: [
+            //         {
+            //             title: '类型-1',
+            //             count: 100,
+            //             key: '2-1'
+            //         },
+            //         {
+            //             title: '类型-2',
+            //             key: '3-2',
+            //             children: [
+            //                 {
+            //                     title: '类型-2-3',
+            //                     key: '2-3'
+            //                 },
+            //                 {
+            //                     title: '类型-2-4',
+            //                     key: '3-4'
+            //                 }
+            //             ]
+            //         }
+            //     ]
+            // }]
         };
     }
 };
